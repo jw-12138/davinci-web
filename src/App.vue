777bf81046971f57a8fd44a3771be59280ea43f2
Jacky <29943110+jw-12138@users.noreply.github.com>
2023-03-20 16:04:54 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -327,18 +327,18 @@ export default {
           model: 'gpt-3.5-turbo',
           url: '/chat/chat-gpt'
         },
-        {
-          name: 'GPT-4 8k',
-          model: 'gpt-4',
-          url: '/chat/gpt-4',
-          disabled: true
-        },
-        {
-          name: 'GPT-4 32k',
-          model: 'gpt-4-32k',
-          url: '/chat/gpt-4',
-          disabled: true
-        }
+        // {
+        //   name: 'GPT-4 8k',
+        //   model: 'gpt-4',
+        //   url: '/chat/gpt-4',
+        //   disabled: true
+        // },
+        // {
+        //   name: 'GPT-4 32k',
+        //   model: 'gpt-4-32k',
+        //   url: '/chat/gpt-4',
+        //   disabled: true
+        // }
       ],
       checkingLogin: false,
       systemStartTime: Date.now(),
